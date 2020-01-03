c999a8376570a5bf9111e962df3272b69d0ff1d7
D3stros <patrick.fallmann@hotmail.com>
2020-01-03 22:58:12 +0100

    Added unit tests for card game

--- card_game.py
+++ card_game.py
@@ -13,8 +13,8 @@ class Card:
 class Deck:
     def __init__(self):
         suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
-        values = ["A", "2", "3", "4", "5", "6",
-                  "7", "8", "9", "10", "J", "Q", "K"]
+        values = ['A', '2', '3', '4', '5', '6',
+                  '7', '8', '9', '10', 'J', 'Q', 'K']
         self.cards = [Card(value, suit) for suit in suits for value in values]
 
     def __repr__(self):
@@ -41,16 +41,6 @@ class Deck:
     def shuffle(self):
         if self.count() < 52:
             raise ValueError("Only full decks can be shuffled")
+
         shuffle(self.cards)
         return self
-
-
-d = Deck()
-d.shuffle()
-card = d.deal_card()
-print(card)
-hand = d.deal_hand(50)
-card2 = d.deal_card()
-print(card2)
-print(d.cards)
-card2 = d.deal_card()
